7188b8e872b1b2ac4f43b28500bd858f44cc6520
Ubuntu <ubuntu@ip-172-31-42-178.us-west-2.compute.internal>
2013-08-08 00:03:26 +0000

    Reset web.js

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
+
 var file = fs.readFileSync('index.html', 'utf8');
 var print = file.toString('utf8');
-
-Saved
 
 app.use(express.logger());
 
 app.get('/', function(request, response); {
-  response.send('print');
+  response.send("Hello World!');
+
 });
 
 var port = process.env.PORT || 5000;
