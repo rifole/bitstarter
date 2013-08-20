00b8e226e9cba6b59b5c6f11ac32beb60fd44f93
rifole <rifole@gmail.com>
2013-08-20 15:36:01 +0000

    multi page

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+var about_html = 'about.html';
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    response.send(fs.readFileSync('index.html').toString());
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
+
+app.get('/about', function(req,res){
+    res.send(fs.readFileSync(about_html).toString
 });
 
 var port = process.env.PORT || 8080
