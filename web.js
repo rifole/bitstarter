56be5d52018df7c05577697f8168f837b106f535
Ubuntu <ubuntu@ip-172-31-46-62.us-west-2.compute.internal>
2013-09-02 13:41:01 +0000

    multiple forms

--- web.js
+++ web.js
@@ -5,6 +5,8 @@ var about_html = 'about.html';
 var contact_html = 'contact.html';
 var services_html = 'services.html';
 var start_will_html = 'start_will.html';
+var start_trust_html = 'start_trust.html';
+var start_contact_html = 'start_contact.html';
 
 var app = express.createServer(express.logger());
 
@@ -27,6 +29,13 @@ app.get('/services', function(req,res){
 
 app.get('/start_will', function(req,res){
     res.send(fs.readFileSync(start_will_html).toString());
+});
+
+app.get('/start_trust', function(req,res){
+    res.send(fs.readFileSync(start_trust_html).toString());
+});
+app.get('/start_contact', function(req,res){
+    res.send(fs.readFileSync(start_contact_html).toString());
 });
 
 var port = process.env.PORT || 8080
